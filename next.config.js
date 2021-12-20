51874705ba710df979d1e7b6b0c84893467e64f9
Moaaz Abbas <moaazabbas16@gmail.com>
2021-12-20 21:28:49 +0200

    refactoring

--- next.config.js
+++ next.config.js
@@ -1,20 +1,3 @@
-/* const {
-	PHASE_DEVELOPMENT_SERVER,
-	PHASE_PRODUCTION_SERVER,
-	PHASE_PRODUCTION_BUILD,
-} = require('next/constants');
-
-module.exports = () => {
-	if (PHASE_PRODUCTION_BUILD || PHASE_PRODUCTION_SERVER)
-		return {
-			env : {
-				GOOGLE_ID     :
-					'632197140735-2vmr18dijktelm5nqt4hottt11je1nhn.apps.googleusercontent.com',
-				GOOGLE_SECRET : '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',
-			},
-		};
-}; */
-
 module.exports = {
 	images          : {
 		domains : [
