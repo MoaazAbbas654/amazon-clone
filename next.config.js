e3c07507868db4046e02d21efc3772b46fdf1f7b
Moaaz Abbas <moaazabbas16@gmail.com>
2021-12-20 15:48:01 +0200

    fixing production error

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+const {
+	PHASE_DEVELOPMENT_SERVER,
+	PHASE_PRODUCTION_SERVER,
+	PHASE_PRODUCTION_BUILD,
+} = require('next/constants');
+
+module.exports = () => {
+	if (PHASE_PRODUCTION_BUILD || PHASE_PRODUCTION_SERVER)
+		return {
+			env : {
+				GOOGLE_ID     :
+					'632197140735-2vmr18dijktelm5nqt4hottt11je1nhn.apps.googleusercontent.com',
+				GOOGLE_SECRET : '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',
+			},
+		};
+};
+
 module.exports = {
 	images          : {
 		domains : [
@@ -5,11 +22,6 @@ module.exports = {
 			'fakestoreapi.com',
 			'm.media-amazon.com',
 		],
-		env     : {
-			GOOGLE_ID     :
-				'632197140735-2vmr18dijktelm5nqt4hottt11je1nhn.apps.googleusercontent.com',
-			GOOGLE_SECRET : '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',
-		},
 	},
 	reactStrictMode : true,
 };
